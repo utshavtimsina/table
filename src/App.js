187f07109fabaae44428e08c5ea7d08e459a5c7f
Abhinay Shrestha <abhinay.shrestha@ekbana.info>
2021-03-13 18:51:37 +0545

    Restructure axios

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import ReactTable from "react-table"; 
-import 'react-table/react-table.css'
+import React, { Component } from "react";
+import axios from "axios";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      records: [],
-      loading:true
-    }
+  state = {
+    records: [],
+    loading: true,
+  };
+
+  async getUsersData() {
+    const res = await axios.get("/client-records/find/all");
+    this.setState({ loading: false, records: res.data });
   }
-  async getUsersData(){
-    const res = await axios.get('http://hosting.iptvnepal.net:8081/client-records/find/all')
-    console.log(res.data)
-    this.setState({loading:false, records: res.data})
+
+  componentDidMount() {
+    this.getUsersData();
   }
-  componentDidMount(){
-    this.getUsersData()
-  }
+
   render() {
-    const columns = [{  
-      Header: 'ID',  
-      accessor: 'id'
-     }
-     ,{  
-      Header: 'Name',  
-      accessor: 'clientFullName'
-      }
-     
-     ,{  
-     Header: 'Domain',  
-     accessor: 'clientDomain'
-     }
-     ,{  
-     Header: 'Phone',  
-     accessor: 'clientContactNo'
-     },
-     {  
-      Header: 'Email',  
-      accessor: 'clientEmail'
+    const columns = [
+      {
+        Header: "ID",
+        accessor: "id",
       },
-	{
-		Header: 'Contract Date',
-		accessor: 'contractDate'
-	}
-  ]
-    return (
-      <ReactTable  
-      data={this.state.records}  
-      columns={columns}  
-   />
-    )
+      {
+        Header: "Name",
+        accessor: "clientFullName",
+      },
+
+      {
+        Header: "Domain",
+        accessor: "clientDomain",
+      },
+      {
+        Header: "Phone",
+        accessor: "clientContactNo",
+      },
+      {
+        Header: "Email",
+        accessor: "clientEmail",
+      },
+      {
+        Header: "Contract Date",
+        accessor: "contractDate",
+      },
+    ];
+    return <ReactTable data={this.state.records} columns={columns} />;
   }
 }
-
